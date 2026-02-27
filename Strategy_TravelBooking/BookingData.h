b9f3757687d0ca58355487332aa6d3e5675c92b9
una-07 <esirkepulpan8@gmail.com>
2026-02-27 20:18:03 +0500

    Create BookingData.h

---
+++ Strategy_TravelBooking/BookingData.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct BookingData {
+    double distance;
+    int passengers;
+    bool businessClass;
+    bool childDiscount;
+    bool пенсионerDiscount;
+    int baggageCount;
+};
